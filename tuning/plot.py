4bffba0876be1a2ae021a33372aaa72386455fb8
Wolfgang Hoenig <hoenig@tu-berlin.de>
2022-01-25 21:54:05 +0100

    usd logging: visualize absolute position

--- tuning/plot.py
+++ tuning/plot.py
@@ -2,7 +2,6 @@ import argparse
 
 import matplotlib.pyplot as plt
 import numpy as np
-import rowan
 
 import cfusdlog
 
@@ -46,36 +45,47 @@ if __name__ == '__main__':
         data_usd['fixedFrequency']['ctrlMel.ew_z'],
     ]).T
 
-    fig, ax = plt.subplots(4, 3, sharex='all', sharey='row')
-    ax[0,0].plot(time_fF, ep[:,0], label='ep.x')
-    ax[0,1].plot(time_fF, ep[:,1], label='ep.y')
-    ax[0,2].plot(time_fF, ep[:,2], label='ep.z')
-    ax[0,0].set_ylabel('Position error [m]')
+    time_eP = (data_usd['estPosition']['timestamp'] - start_time) / 1e3
+    pos_mocap = np.array([
+        data_usd['estPosition']['locSrv.x'],
+        data_usd['estPosition']['locSrv.y'],
+        data_usd['estPosition']['locSrv.z'],
+    ]).T
 
-    ax[1,0].plot(time_fF, ev[:,0], label='ev.x')
-    ax[1,1].plot(time_fF, ev[:,1], label='ev.y')
-    ax[1,2].plot(time_fF, ev[:,2], label='ev.z')
-    ax[1,0].set_ylabel('Velocity error [m/s]')
+    pos_ekf = np.array([
+        data_usd['fixedFrequency']['stateEstimateZ.x'],
+        data_usd['fixedFrequency']['stateEstimateZ.y'],
+        data_usd['fixedFrequency']['stateEstimateZ.z'],
+    ]).T / 1000.0
 
-    ax[2,0].plot(time_fF, eR[:,0], label='eR.x')
-    ax[2,1].plot(time_fF, eR[:,1], label='eR.y')
-    ax[2,2].plot(time_fF, eR[:,2], label='eR.z')
-    ax[2,0].set_ylabel('Angular error [?]')
+    fig, ax = plt.subplots(5, 3, sharex='all', sharey='row')
+    ax[0,0].plot(time_fF, pos_ekf[:,0], label='EKF')
+    ax[0,0].scatter(time_eP, pos_mocap[:,0], label='mocap')
+    ax[0,1].plot(time_fF, pos_ekf[:,1], label='EKF')
+    ax[0,0].scatter(time_eP, pos_mocap[:,1], label='mocap')
+    ax[0,2].plot(time_fF, pos_ekf[:,2], label='EKF')
+    ax[0,0].scatter(time_eP, pos_mocap[:,2], label='mocap')
+    ax[0,0].set_ylabel('Position [m]')
 
-    ax[3,0].plot(time_fF, ew[:,0], label='ew.x')
-    ax[3,1].plot(time_fF, ew[:,1], label='ew.y')
-    ax[3,2].plot(time_fF, ew[:,2], label='ew.z')
-    ax[3,0].set_ylabel('Angular velocity error [rad/s]')
+    ax[1,0].plot(time_fF, ep[:,0], label='ep.x')
+    ax[1,1].plot(time_fF, ep[:,1], label='ep.y')
+    ax[1,2].plot(time_fF, ep[:,2], label='ep.z')
+    ax[1,0].set_ylabel('Position error [m]')
 
-    # ax[0].legend()
+    ax[2,0].plot(time_fF, ev[:,0], label='ev.x')
+    ax[2,1].plot(time_fF, ev[:,1], label='ev.y')
+    ax[2,2].plot(time_fF, ev[:,2], label='ev.z')
+    ax[2,0].set_ylabel('Velocity error [m/s]')
 
-    # ax[1].plot(time_fF, np.degrees(mocap_rpy[:, 1]), label='mocap')
-    # ax[1].plot(time_fF, np.degrees(cf_rpy[:, 1]), label='state estimate')
+    ax[3,0].plot(time_fF, eR[:,0], label='eR.x')
+    ax[3,1].plot(time_fF, eR[:,1], label='eR.y')
+    ax[3,2].plot(time_fF, eR[:,2], label='eR.z')
+    ax[3,0].set_ylabel('Angular error [?]')
 
-    # ax[2].plot(time_fF, np.degrees(mocap_rpy[:, 2]), label='mocap')
-    # ax[2].plot(time_fF, np.degrees(cf_rpy[:, 2]), label='state estimate')
-
-    plt.legend()
+    ax[4,0].plot(time_fF, ew[:,0], label='ew.x')
+    ax[4,1].plot(time_fF, ew[:,1], label='ew.y')
+    ax[4,2].plot(time_fF, ew[:,2], label='ew.z')
+    ax[4,0].set_ylabel('Angular velocity error [rad/s]')
 
     plt.show()
 
